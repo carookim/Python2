6efaa6e47b90b145c1312184f68f756251915238
carookim <carookim77@gmail.com>
2025-10-30 08:52:52 +0900

    20251030

--- ll.py
+++ ll.py
@@ -1,3 +1,25 @@
 # curl -O https://raw.githubusercontent.com/사용자명/리포지토리명/브랜치명/파일경로/파일이름.py
 
 # wget https://raw.githubusercontent.com/사용자명/리포지토리명/브랜치명/파일경로/파일이름.py
+
+https://raw.githubusercontent.com/pia222sk20/python/refs/heads/main/%EB%94%A5%EB%9F%AC%EB%8B%9D/model11.ipynb
+
+curl -O https://raw.githubusercontent.com/pia222sk20/python/refs/heads/main/%EB%94%A5%EB%9F%AC%EB%8B%9D/model11.ipynb
+
+4
+https://github.com/pia222sk20/python/blob/main/%EB%94%A5%EB%9F%AC%EB%8B%9D/model4.ipynb
+
+5
+https://github.com/pia222sk20/python/blob/main/%EB%94%A5%EB%9F%AC%EB%8B%9D/model5.ipynb
+
+6
+https://github.com/pia222sk20/python/blob/main/%EB%94%A5%EB%9F%AC%EB%8B%9D/model6.ipynb
+
+7
+https://github.com/pia222sk20/python/blob/main/%EB%94%A5%EB%9F%AC%EB%8B%9D/model7.ipynb
+
+8
+https://github.com/pia222sk20/python/blob/main/%EB%94%A5%EB%9F%AC%EB%8B%9D/model8.ipynb
+
+9
+https://raw.githubusercontent.com/pia222sk20/python/refs/heads/main/%EB%94%A5%EB%9F%AC%EB%8B%9D/model9.ipynb
